79b656c5afa455784abb4b3fee0b6ba575cf9783
Nikolas Lopes <nhikolass87@gmail.com>
2026-04-07 16:55:08 -0300

    Adiciona programa de maior ou menor

---
+++ MaiorOuMenor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MaiorOuMenor {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: ");
+        int numero1 = input.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        int numero2 = input.nextInt();
+
+        if(numero1 > numero2) {
+            System.out.println("Maior:" + numero1);
+
+        } else if (numero2 > numero1) {
+            System.out.println("Maior: " + numero2);
+        } else {
+            System.out.println("Os números são iguais");
+
+        }
+
+        input.close();
+    }
+}
---
+++ Model.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Model {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        
+
+        input.close();
+    }
+}
