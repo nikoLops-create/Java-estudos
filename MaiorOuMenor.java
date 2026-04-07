a19cdd919c0810a86f4c4e376d8157c4e653bdcd
Nikolas Lopes <nhikolass87@gmail.com>
2026-04-07 19:23:16 -0300

    Corrige formatação do if

--- MaiorOuMenor.java
+++ MaiorOuMenor.java
@@ -12,8 +12,7 @@ public class MaiorOuMenor {
         System.out.print("Digite o segundo numero: ");
         int numero2 = input.nextInt();
 
-        if(numero1 >
-             numero2) {
+        if(numero1 > numero2) {
             System.out.println("Maior:" + numero1);
 
         } else if (numero2 > numero1) {
