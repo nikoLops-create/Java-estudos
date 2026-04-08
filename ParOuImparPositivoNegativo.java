eaa3a554c55ef3a7244ab26d1189eba8f5454606
Nikolas Lopes <nhikolass87@gmail.com>
2026-04-07 22:03:50 -0300

    Adiciona código de par/impar e positivo/negativo

---
+++ ParOuImparPositivoNegativo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ParOuImparPositivoNegativo {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int numero = input.nextInt();
+
+        if (numero % 2==0) {
+            System.out.println("Par");
+        } else {
+            System.out.println("Impar");
+
+        }
+
+        if (numero > 0) {
+            System.out.println("Positivo");
+        } else if ( numero < 0 ) { 
+            System.out.println("Negativo");
+        } else {
+            System.out.println("Zero");
+
+        }
+
+
+input.close();
+
+
+    }
+
+
+}
